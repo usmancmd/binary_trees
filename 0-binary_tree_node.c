d7cb6b62ea66f76e1576290a880ed48c56a74ac3
Usman Adepoju <adepojuusman465@gmail.com>
2023-03-02 08:09:46 +0000

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,7 +9,8 @@
  *
  * Return: pointer to the new node, or NULL on failure
  */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
 	binary_tree_t *new_node;
 
   	new_node = malloc(sizeof(binary_tree_t));
