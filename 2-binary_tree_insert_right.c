720044b00eb57e0c2d752efe76eb65e94c654458
Usman Adepoju <adepojuusman465@gmail.com>
2023-03-02 09:56:40 +0000

    2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,11 +12,11 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	binary_tree_t *new;
 
 	if (parent == NULL)
-	return (NULL);
+		return (NULL);
 
 	new = binary_tree_node(parent, value);
 	if (new == NULL)
-	return (NULL);
+		return (NULL);
 
 	if (parent->right != NULL)
 	{
